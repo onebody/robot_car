fd127d2d4f2db07eb5d986b66f9b0c7d0dad66a6
onebody <onebodysoftware@hotmail.com>
2014-09-24 00:30:38 +0800

    PiSoftPWM  软件方式实现PWM

---
+++ RPi_PWM/PiSoftPwm/PiSoftPwm.py
@@ -0,0 +1,102 @@
+# The original is aboudou ,the Source code is here : https://goddess-gate.com/dc2/index.php/pages/raspiledmeter.en
+# The modifier is ukonline2000
+
+import RPi.GPIO as GPIO
+import threading
+import time
+ 
+class PiSoftPwm(threading.Thread):
+
+  def __init__(self, baseTime, nbSlices, gpioPin, gpioScheme):
+     """ 
+     Init the PiSoftPwm instance. Expected parameters are :
+     - baseTime : the base time in seconds for the PWM pattern. You may choose a small value (i.e 0.01 s)
+     - nbSlices : the number of divisions of the PWM pattern. A single pulse will have a min duration of baseTime * (1 / nbSlices)
+     - gpioPin : the pin number which will act as PWM ouput
+     - gpioScheme : the GPIO naming scheme (see RPi.GPIO documentation)
+     """
+     self.sliceTime = baseTime / nbSlices
+     self.baseTime = baseTime
+     self.nbSlices = nbSlices
+     self.gpioPin = gpioPin
+     self.terminated = False
+     self.toTerminate = False
+     GPIO.setmode(gpioScheme)
+
+  def start(self, nbSlicesOn):
+    """
+    Start PWM output. Expected parameter is :
+    - nbSlicesOn : number of divisions (on a total of nbSlices - see init() doc) to set HIGH output on the GPIO pin
+    
+    Exemple : with a total of 100 slices, a baseTime of 1 second, and an nbSlicesOn set to 25, the PWM pattern will
+    have a duty cycle of 25%. With a duration of 1 second, will stay HIGH for 1*(25/100) seconds on HIGH output, and
+    1*(75/100) seconds on LOW output.
+    """
+    self.nbSlicesOn = nbSlicesOn
+    GPIO.setup(self.gpioPin, GPIO.OUT)
+    self.thread = threading.Thread(None, self.run, None, (), {})
+    self.thread.start()
+
+  def run(self):
+    """
+    Run the PWM pattern into a background thread. This function should not be called outside of this class.
+    """
+    while self.toTerminate == False:
+      if self.nbSlicesOn > 0:
+        GPIO.output(self.gpioPin, GPIO.HIGH)
+        time.sleep(self.nbSlicesOn * self.sliceTime)
+      if self.nbSlicesOn < self.nbSlices:
+        GPIO.output(self.gpioPin, GPIO.LOW)
+        time.sleep((self.nbSlices - self.nbSlicesOn) * self.sliceTime)
+    self.terminated = True
+
+  def changeNbSlicesOn(self, nbSlicesOn):
+    """
+    Change the duration of HIGH output of the pattern. Expected parameter is :
+    - nbSlicesOn : number of divisions (on a total of nbSlices - see init() doc) to set HIGH output on the GPIO pin
+    
+    Exemple : with a total of 100 slices, a baseTime of 1 second, and an nbSlicesOn set to 25, the PWM pattern will
+    have a duty cycle of 25%. With a duration of 1 second, will stay HIGH for 1*(25/100) seconds on HIGH output, and
+    1*(75/100) seconds on LOW output.
+    """
+    self.nbSlicesOn = nbSlicesOn
+
+  def changeNbSlices(self, nbSlices):
+    """
+    Change the number of slices of the PWM pattern. Expected parameter is :
+    - nbSlices : number of divisions of the PWM pattern.
+    
+    Exemple : with a total of 100 slices, a baseTime of 1 second, and an nbSlicesOn set to 25, the PWM pattern will
+    have a duty cycle of 25%. With a duration of 1 second, will stay HIGH for 1*(25/100) seconds on HIGH output, and
+    1*(75/100) seconds on LOW output.
+    """
+    if self.nbSlicesOn > nbSlices:
+      self.nbSlicesOn = nbSlices
+
+    self.nbSlices = nbSlices
+    self.sliceTime = self.baseTime / self.nbSlices
+
+  def changeBaseTime(self, baseTime):
+    """
+    Change the base time of the PWM pattern. Expected parameter is :
+    - baseTime : the base time in seconds for the PWM pattern.
+    
+    Exemple : with a total of 100 slices, a baseTime of 1 second, and an nbSlicesOn set to 25, the PWM pattern will
+    have a duty cycle of 25%. With a duration of 1 second, will stay HIGH for 1*(25/100) seconds on HIGH output, and
+    1*(75/100) seconds on LOW output.
+    """
+    self.baseTime = baseTime
+    self.sliceTime = self.baseTime / self.nbSlices
+
+
+  def stop(self):
+    """
+    Stops PWM output.
+    """
+    self.toTerminate = True
+    while self.terminated == False:
+      # Just wait
+      time.sleep(0.01)
+  
+    GPIO.output(self.gpioPin, GPIO.LOW)
+    GPIO.setup(self.gpioPin, GPIO.IN)
